96e04e05d44e0d281ed8e271df63aa823a1e1cf3
Andrey Nekrasov <bonerdelli@gmail.com>
2022-05-13 21:47:49 +0300

    Copy direct edits from production server

--- app/Providers/SocialiteProviders/Geobuilder/GeobuilderProvider.php
+++ app/Providers/SocialiteProviders/Geobuilder/GeobuilderProvider.php
@@ -10,17 +10,50 @@ class GeobuilderProvider extends AbstractProvider
     /**
      * Unique Provider Identifier.
      */
-    const IDENTIFIER = 'SOCIALITE_GEOBUILDER';
+    public const IDENTIFIER = 'SOCIALITE_GEOBUILDER';
 
+    /**
+     * Base OAuth service URL
+     * TODO: move out to config?
+     *
+     * @var string
+     */
     private const URL = 'https://fs.geobuilder.ru/idp/connect';
 
+    /**
+     * Indicates if the session state should be utilized.
+     *
+     * @var bool
+     */
+    protected $stateless = true;
+
+    // protected $scopeSeparator = ' ';
+    // protected $scopes = ['openid'];
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function additionalConfigKeys()
+    {
+        return ['client_secret', 'nonce', 'realms'];
+    }
+
+    // protected function getBaseUrl()
+    // {
+    //     return rtrim(rtrim($this->getConfig('base_url'), '/').'/realms/'.$this->getConfig('realms', 'master'), '/');
+    // }
+
+    protected function getBaseUrl()
+    {
+        return self::URL;
+    }
 
     /**
      * {@inheritdoc}
      */
     protected function getAuthUrl($state)
     {
-        return $this->buildAuthUrlFromBase(self::URL.'/authorize', $state);
+        return $this->buildAuthUrlFromBase($this->getBaseUrl().'/authorize', $state);
     }
 
     /**
@@ -28,22 +61,75 @@ class GeobuilderProvider extends AbstractProvider
      */
     protected function getTokenUrl()
     {
-        return self::URL.'/access_token';
+        return $this->getBaseUrl().'/token';
     }
+
+    /**
+     * Get the access token response for the given code.
+     *
+     * @param  string  $code
+     * @return array
+     */
+    // public function getAccessTokenResponse($code)
+    // {
+    //     $response = $this->getHttpClient()->post($this->getTokenUrl(), [
+    //         'headers' => RequestOptions::HEADERS => [
+    //             'Authorization' => 'Bearer '.$token,
+    //             'Accept' => 'application/json'
+    //         ],
+    //         'form_params' => $this->getTokenFields($code),
+    //     ]);
+    //
+    //     return json_decode($response->getBody(), true);
+    // }
 
     /**
      * {@inheritdoc}
      */
     protected function getUserByToken($token)
     {
-        $response = $this->getHttpClient()->get(self::URL.'/user', [
-            'headers' => [
+        $response = $this->getHttpClient()->get($this->getBaseUrl().'/userinfo', [
+            RequestOptions::HEADERS => [
                 'Authorization' => 'Bearer '.$token,
             ],
         ]);
 
-        return json_decode($response->getBody(), true);
+        print '<pre>'; print_r($response->getBody()); die;
+        return json_decode((string) $response->getBody(), true);
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id'        => Arr::get($user, 'sub'),
+            'name'      => Arr::get($user, 'name'),
+            'email'     => Arr::get($user, 'email'),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code',
+        ]);
+    }
+
+    /**
+     * Return logout endpoint with redirect_uri query parameter.
+     *
+     * @return string
+     */
+    public function getLogoutUrl(string $redirectUri)
+    {
+        return $this->getBaseUrl().'/logout?redirect_uri='.urlencode($redirectUri);
+    }
+
 
     /**
       * Get the GET parameters for the code request.
@@ -64,20 +150,4 @@ class GeobuilderProvider extends AbstractProvider
 
          return array_merge($fields, $this->parameters);
      }
-
-
-
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function mapUserToObject(array $user)
-    {
-        return (new User())->setRaw($user['user'])->map([
-            'id'       => $user['user']['uid'],
-            'nickname' => $user['user']['username'],
-            'name'     => $user['user']['name'],
-            'email'    => $user['user']['email'],
-        ]);
-    }
 }
